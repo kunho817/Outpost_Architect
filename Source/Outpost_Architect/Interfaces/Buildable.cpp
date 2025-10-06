83ac252853b8605449789528abf12432e7bebe6d
BMO Liper Okbul <kunho817@naver.com>
2025-10-06 17:06:36 +0900

    게임 모드 / 웨이브 기능 추가

---
+++ Source/Outpost_Architect/Interfaces/Buildable.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interfaces/Buildable.h"
+
+// Add default functionality here for any IBuildable functions that are not pure virtual.
