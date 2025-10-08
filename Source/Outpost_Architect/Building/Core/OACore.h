1d8598e02b5d9abff797bf6ebb6d1b012855ef83
BMO Liper Okbul <kunho817@naver.com>
2025-10-09 00:00:35 +0900

    Base Core 추가.

---
+++ Source/Outpost_Architect/Building/Core/OACore.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Building/OABuildingBase.h"
+#include "OACore.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OUTPOST_ARCHITECT_API AOACore : public AOABuildingBase
+{
+	GENERATED_BODY()
+	
+};
