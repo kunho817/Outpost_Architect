32d6d744f93c9586a1f09be1dd2bbc62de56dd99
BMO Liper Okbul <kunho817@naver.com>
2025-10-09 02:21:32 +0900

    AI 개편 및 터렛과 적 인식 수정 #1

---
+++ Source/Outpost_Architect/AI/Task/Task_AttackObs.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "Task_AttackObs.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OUTPOST_ARCHITECT_API UTask_AttackObs : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+};
