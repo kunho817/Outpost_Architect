75e9a028b51739716844f0629563d1381ed2d3ac
BMO Liper Okbul <kunho817@naver.com>
2025-10-10 02:57:05 +0900

    적 AI 수정

---
+++ Source/Outpost_Architect/AI/Service/Service_CheckObs.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTService.h"
+#include "Service_CheckObs.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OUTPOST_ARCHITECT_API UService_CheckObs : public UBTService
+{
+	GENERATED_BODY()
+	
+};
