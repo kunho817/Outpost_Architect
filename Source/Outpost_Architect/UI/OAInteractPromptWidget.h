f266c67f66457ec31b6ca7ff8e56d7d5327685a9
BMO Liper Okbul <kunho817@naver.com>
2025-10-25 00:47:09 +0900

    기반 건물, 자동화 기능 구현.

---
+++ Source/Outpost_Architect/UI/OAInteractPromptWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/OABaseWidget.h"
+#include "OAInteractPromptWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OUTPOST_ARCHITECT_API UOAInteractPromptWidget : public UOABaseWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Outpost_Architect/UI/OAInventoryWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "OAInventoryWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OUTPOST_ARCHITECT_API UOAInventoryWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
