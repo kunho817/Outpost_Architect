4a6021aa1e420b2a13845387ff2675c256038f98
BMO Liper Okbul <kunho817@naver.com>
2025-10-28 02:41:40 +0900

    베이스 생산 건물 추가

---
+++ Source/Outpost_Architect/Building/Production/OAProductionBuildingBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Building/OABuildingBase.h"
+#include "OAProductionBuildingBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OUTPOST_ARCHITECT_API AOAProductionBuildingBase : public AOABuildingBase
+{
+	GENERATED_BODY()
+	
+};
